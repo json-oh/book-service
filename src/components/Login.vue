37c07a96103dd0ae1cb367116ff0e38b80edc381
seren.kim <seren.kim@kakaomobility.com>
2020-09-12 17:49:10 +0900

    login ui 작업

---
+++ src/components/Login.vue
@@ -0,0 +1,38 @@
+<template>
+    <amplify-authenticator usernameAlias="email">
+        <amplify-sign-up
+                slot="sign-up"
+                usernameAlias="email"
+                :form-fields.prop="formFields"
+        ></amplify-sign-up>
+        <amplify-sign-in
+                slot="sign-in"
+                usernameAlias="email"
+                :form-fields.prop="formFields"
+        ></amplify-sign-in>
+        <amplify-sign-out></amplify-sign-out>
+    </amplify-authenticator>
+</template>
+<script>
+    export default {
+        name : "login",
+        data : function () {
+            return {
+                formFields : [
+                    {
+                        type: "email",
+                        label: "Email",
+                        placeholder: "email@aws.com",
+                        required: true,
+                    },
+                    {
+                        type: "password",
+                        label: "Password",
+                        placeholder: "password",
+                        required: true,
+                    }
+                ]
+            }
+        }
+    }
+</script>
