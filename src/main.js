b0c98d9bdc58fbc4d5702427a5935e586edb2bd5
Jisan Oh <json.oh@woowahan.com>
2020-09-12 22:03:56 +0900

    텍스트 인식 backend 추가

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import Vue from "vue";
 import App from "./App.vue";
 import Amplify from "aws-amplify";
 import "@aws-amplify/ui-vue";
+import { AmazonAIPredictionsProvider } from '@aws-amplify/predictions';
 import aws_exports from "./aws-exports";
 
 Amplify.configure(aws_exports);
+Amplify.addPluggable(new AmazonAIPredictionsProvider());
 
 Vue.config.productionTip = false;
 
