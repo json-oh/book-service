a4ed3225ec3eb6d94f0c158b0f3e25d24f39623a
seren.kim <seren.kim@kakaomobility.com>
2020-09-16 17:28:30 +0900

    로그인화면에 로고 추가

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,12 +1,15 @@
 <template>
-  <amplify-authenticator username-alias="email">
-    <amplify-sign-up
-      slot="sign-up"
-      username-alias="email"
-      :form-fields.prop="formFields"
-    ></amplify-sign-up>
-    <amplify-sign-in slot="sign-in" username-alias="email"></amplify-sign-in>
-  </amplify-authenticator>
+  <div>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <amplify-authenticator username-alias="email">
+      <amplify-sign-up
+        slot="sign-up"
+        username-alias="email"
+        :form-fields.prop="formFields"
+      ></amplify-sign-up>
+      <amplify-sign-in slot="sign-in" username-alias="email"></amplify-sign-in>
+    </amplify-authenticator>
+  </div>
 </template>
 
 <script>
